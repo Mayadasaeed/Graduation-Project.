fbfe636cc46c99922d701c48cb83986c743948a4
Mayada Saeed <Mayadasaeed812@gmail.com>
2024-07-19 18:44:21 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+
+
+
+
+  return (
+    <>
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
+
+
+
